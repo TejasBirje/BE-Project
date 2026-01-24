b6fe5b77e105c7ca8c7dcb1145b3d10bb335b3e9
Tejas Birje <tejasbirje30@gmail.com>
2026-01-24 17:43:18 +0530

    home page

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Analytics from './components/Analytics'
+import Footer from './components/Footer'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='min-h-screen'>
+      <Header />
+      <Hero />
+      <Features />
+      <Analytics />
+      <Footer />
+    </div>
   )
 }
 
